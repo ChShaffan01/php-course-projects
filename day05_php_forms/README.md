6dedae59fbedaace201a804a1cdc94e3f749cc81
CH_Shaffan <chshaffan27@gmail.com>
2026-01-30 00:51:03 +0500

    Add files via upload

---
+++ day05_php_forms/README.md
@@ -0,0 +1,3 @@
+# day05_php_forms
+
+Handling HTML forms in PHP using GET and POST methods.
---
+++ day05_php_forms/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST["name"];
+$password = $_POST["pwd"];
+
+echo "Name: " . $name . "\n";
+echo "Password: " . $password . "\n";
+
+
+
+?>
